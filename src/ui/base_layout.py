52016f0d026115c1927dcdcd3feaadb72bb68cf9
Sudarshan <jhasudarshan2005@gmail.com>
2026-05-15 17:11:57 +0530

    commit

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -22,7 +22,8 @@ def style_background_home():
             ,unsafe_allow_html=True)
     
 
-def style_background_dashboard(): 
+def style_background_dashboard():
+
     st.markdown("""
         <style>
 
@@ -59,7 +60,7 @@ def style_base_layout():
             h1 {
                 font-family: 'Climate Crisis', sans-serif !important;
                 font-size: 3.5rem !important;
-                line-height:1.1 !important;
+                line-height:1.1 1important;
                 margin-bottom:0rem !important;
             }
                 
@@ -69,7 +70,6 @@ def style_base_layout():
                 font-size: 2rem !important;
                 line-height:0.9 !important;
                 margin-bottom:0rem !important;
-                color:black !important;
             }
                 
             h3, h4, p {
@@ -105,24 +105,7 @@ def style_base_layout():
                 }
 
             button:hover{
-                transform :scale(1.05);
-            }
-            .stTextInput input {
-                color: black !important;
-                background-color: white !important;
-            }
-
-            .stTextArea textarea {
-               color: black !important;
-               background-color: white !important;
-            }
-
-label {
-    color: black !important;
-}
-                
-
-            
+                transform :scale(1.05)}
         </style>  
 
                 """
