b1f3d5c9dfa34c56f12ab3498007dd3ba85701dd
Sudarshan <jhasudarshan2005@gmail.com>
2026-05-15 17:16:24 +0530

    commit

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -14,17 +14,28 @@ def home_screen():
     col1, col2 = st.columns(2, gap="large")
 
     with col1:
-        st.header("I'm Student")
+        st.markdown("<h2 style='color:black;'>I'm Student</h2>", unsafe_allow_html=True)
         st.image("https://i.ibb.co/844D9Lrt/mascot-student.png", width=120)
-        if st.button('Student Portal', type='primary', icon=':material/arrow_outward:', icon_position='right'):
-            st.session_state['login_type']='student'
+        if st.button(
+            'Student Portal',
+             type='primary',
+            icon=':material/arrow_outward:',
+            icon_position='right'
+        ):
+            st.session_state['login_type'] = 'student'
             st.rerun()
 
     with col2:
-        st.header("I'm Teacher")
+        st.markdown("<h2 style='color:black;'>I'm Teacher</h2>", unsafe_allow_html=True)
         st.image("https://i.ibb.co/CsmQQV6X/mascot-prof.png", width=145)
-        if st.button('Teacher Portal', type='primary', icon=':material/arrow_outward:', icon_position='right'):
-            st.session_state['login_type']='teacher'
+
+        if st.button(
+        'Teacher Portal',
+        type='primary',
+        icon=':material/arrow_outward:',
+        icon_position='right'
+        ):
+            st.session_state['login_type'] = 'teacher'
             st.rerun()
 
     footer_home()
